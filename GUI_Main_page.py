68c1b7cd19f18145babd74a8c3d10aa0ca1ffa0a
JPJ-WM <jack.pj@live.co.uk>
2019-02-05 13:58:32 +0000

    Added config read

--- GUI_Main_page.py
+++ GUI_Main_page.py
@@ -1,3 +1,13 @@
 """
 Builds the main page of the rescue screen
 """
+# Imports
+import tkinter
+# Import tkinter.font
+import tkinter.ttk as ttk
+import configparser
+
+# Get and read config
+config = configparser.ConfigParser()
+config.read('Config/config.ini')
+
